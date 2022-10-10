086875a1d2cc9ca9221675e9858f270d99bc959a
Satang Budsai <Satang_Budsai@cmu.ac.th>
2022-10-10 17:06:54 +0700

    ttt

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,8 @@ module.exports = function (config) {
       jasmine: {
         // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // for example, you can disable the random execution with random: false
+        // or set a specific seed with seed: 4321
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -37,12 +37,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+<<<<<<< HEAD
     browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+=======
+    browsers: ['Chrome', "ChromeHeadless", 'ChromeHeadlessCI'],
+>>>>>>> f07be43c (ttt)
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
+<<<<<<< HEAD
       },
+=======
+      }
+>>>>>>> f07be43c (ttt)
     },
     singleRun: false,
     restartOnFileChange: true
